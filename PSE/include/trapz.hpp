5b73940a93c82d4955d886e238ab6a9c07d87228
Shaun Harris <shaun.r.harris@gmail.com>
2018-05-08 17:51:52 -0700

    Added function to trapz, still need to add it to Closure functions

--- PSE/include/trapz.hpp
+++ PSE/include/trapz.hpp
@@ -3,14 +3,26 @@
 #include <petscksp.h>
 namespace PSE{
     /**
-     * \brief trapezoidal rule on Vec
+     * \brief trapezoidal rule on Vec with ny values
      * \return 0 if successful
      */
     int trapz(
             Vec &q,             ///< Vector to integrate
             const PetscInt &n,  ///< size of vector
             PetscScalar &I,    ///< output value of integration 
-            const PetscScalar &Deltax=1. ///< Domain size (b-a) in \f$ I = \int_a^b f(x) dx\f$
+            const PetscScalar &Deltay=1. ///< Domain size (b-a) in \f$ I = \int_a^b f(x) dx\f$
+            );
+    /**
+     * \brief trapezoidal rule on Vec with ny,nz values
+     * \return 0 if successful
+     */
+    int trapz(
+            Vec &q,                         ///< Vector to integrate
+            const PetscInt &ny,             ///< size of vector in ny (length(q) = 4*ny*nz)
+            const PetscInt &nz,             ///< size of vector in nz (length(q) = 4*ny*nz)
+            PetscScalar &I,                 ///< output value of integration 
+            const PetscScalar &Deltay=1.,   ///< Domain size (b-a) in y-direction
+            const PetscScalar &Deltaz=1.    ///< Domain size (b-z) in z-direction
             );
 
 
--- PSE/src/trapz.cpp
+++ PSE/src/trapz.cpp
@@ -8,16 +8,16 @@ namespace PSE{
             Vec &q,
             const PetscInt &n,
             PetscScalar &I,
-            const PetscScalar &Deltax ///< Domain size (b-a) in \f[ I = \int_a^b f(x) dx\f]
+            const PetscScalar &Deltay 
             ){
-        // set trapezoidal one row matrix
+        // set trapezoidal column (one row matrix)
         Vec Trap;
         PetscErrorCode ierr;
         Init_Vec(Trap,n);
         PetscInt Istart, Iend;
         VecGetOwnershipRange(Trap,&Istart,&Iend);
-        PetscScalar value= Deltax/((PetscScalar)n);
-        PetscScalar valueBC = Deltax/(2.*(PetscScalar)n);
+        PetscScalar value= Deltay/((PetscScalar)n);
+        PetscScalar valueBC = Deltay/(2.*(PetscScalar)n);
         
         for(int i=Istart; i<Iend; ++i){
             if(i==0 || i==n) {// set ends with half the value
@@ -32,6 +32,59 @@ namespace PSE{
         // I = Trap dot q
         VecDot(Trap,q,&I);
 
+        VecDestroy(&Trap);
+
+        return 0;
+    }
+    int trapz(
+            Vec &q,
+            const PetscInt &ny,
+            const PetscInt &nz,
+            PetscScalar &I,
+            const PetscScalar &Deltay,
+            const PetscScalar &Deltaz
+            ){
+        // set trapezoidal column (one row matrix)
+        Vec Trap;
+        PetscErrorCode ierr;
+        Init_Vec(Trap,ny);
+        PetscInt Istart, Iend;
+        VecGetOwnershipRange(Trap,&Istart,&Iend);
+        PetscScalar value= Deltay/((PetscScalar)ny);
+        PetscScalar valueBC = Deltay/(2.*(PetscScalar)ny);
+        
+        for(int i=Istart; i<Iend; ++i){
+            if(i==0 || i==ny) {// set ends with half the value
+                ierr = VecSetValue(Trap,i,valueBC,INSERT_VALUES);CHKERRQ(ierr);
+            }
+            else{ // set mid values
+                ierr = VecSetValue(Trap,i,value,INSERT_VALUES);CHKERRQ(ierr);
+            }
+        }
+        set_Vec(Trap);//assemble Trapezoidal matrix for y integration
+
+        // I = Trap dot q
+        Vec qsub,qsub_z;
+        Init_Vec(qsub,ny);
+        Init_Vec(qsub_z,nz);
+        for (int zi=0; zi<nz; ++zi){ // for each z plane
+            PetscScalar yi_uvwp = 0;
+            VecZeroEntries(qsub);
+            PetscScalar trap_value;
+            for (int vari=0; vari<4; ++vari){       // for each variable u,v,w,P
+                PetscInt row_eq = (4*zi + vari)*ny;
+                set_Vec(q,row_eq,row_eq+ny,qsub);   // extract a single variable, all y values on z-plane
+                VecDot(Trap,qsub,&trap_value);      // integrate in y
+                yi_uvwp += trap_value;
+            }
+            set_Vec(yi_uvwp,zi,qsub_z); // set integral value into qsub_z vector
+        }
+        set_Vec(qsub_z);// assemble qsub_z
+        trapz(qsub_z,nz,trap_value,Deltaz); // integrate all values in z direction
+
+
+        VecDestroy(&qsub);
+        VecDestroy(&qsub_z);
         VecDestroy(&Trap);
 
         return 0;
